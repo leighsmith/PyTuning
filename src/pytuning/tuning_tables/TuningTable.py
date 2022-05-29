c6712aa2d1e4d380f7a1a1042673b52aae6467af
Leigh Smith <leigh@leighsmith.com>
2022-05-29 14:53:52 -0400

    Corrected missing implicit 1/1 on reading Scala file, unified to a single output method, with a format specifier

--- src/pytuning/tuning_tables/TuningTable.py
+++ src/pytuning/tuning_tables/TuningTable.py
@@ -3,19 +3,24 @@
 import os
 import re
 import sympy as sp
-from pytuning.tuning_tables import create_scala_tuning, create_timidity_tuning
+from pytuning.tuning_tables import *
 
 class TuningTable(object):
     """
     Represents a set of tuned pitches within a tuning system.
     Provides I/O methods for different tuning table formats.
     """
+
+    cents_pattern = re.compile('([0-9]*\.[0-9]*)')
+    rational_pattern = re.compile('([0-9]* *\/ *[0-9]*)')
+    
     def __init__(self):
         self.description = ''
         self.scale = []
-        self.reference_note = 0
-        self.cents_pattern = re.compile('([0-9]*\.[0-9]*)')
-        self.rational_pattern = re.compile('([0-9]* *\/ *[0-9]*)')
+        self.reference_note = None
+
+    def __repr__(self):
+        return "<{} {} {}>".format(self.__class__.__name__, self.scale, self.description)
         
     def read_scala_file(self, file_path):
         """
@@ -27,7 +32,8 @@ class TuningTable(object):
         :rtype: bool
         """
         state = 'read_description' # Uses a state machine for parsing the Scala file.
-        self.scale = []
+        # There is an implicit 1/1 in the Scala file that we make explicit in the scale.
+        self.scale = [sp.core.numbers.Integer(1)]
         expected_scale_values = 0
         if os.path.exists(file_path):
             with open(file_path, 'r') as file_handle:
@@ -61,20 +67,28 @@ class TuningTable(object):
             return True
         return False
 
-    def write_scala_file(self, file_path):
+    def write_file(self, file_path, format = 'Scala'):
         """
-        Writes the tuning table to the named [Scala file format](https://www.huygens-fokker.org/scala/scl_format.html) file.
+        Writes the tuning table to the named file according to the format:
+        
+        - 'Scala': [Scala file format](https://www.huygens-fokker.org/scala/scl_format.html).
+        - 'Timidity': Timidity file format.
+        - 'Csound': Csound tuning declarations.
+        - 'Fluidsynth': Fluidsynth tuning file format.
         """
-        scala_string = create_scala_tuning(self.scale, self.description)
+        # Generate the output as a string, then write it.
+        if format == 'Scala':
+            file_contents = create_scala_tuning(self.scale, self.description)
+        elif format == 'Timidity':
+            file_contents = create_timidity_tuning(self.scale, self.reference_note)
+        elif format == 'Csound':
+            file_contents = create_csound_tuning(self.scale, reference_note = self.reference_note)
+        elif format == 'Fluidsynth':
+            file_contents = create_fluidsynth_tuning(self.scale, reference_note = self.reference_note)
+        else:
+            raise Exception(f"Unknown format {format}")
         with open(file_path, 'w') as file_handle:
-            file_handle.write(scala_string)
+            file_handle.write(file_contents)
+            file_handle.write('\n') # Write final newline.
         return True
 
-    def write_timidity_file(self, file_path):
-        """
-        Writes the tuning table to the named Timidity file.
-        """
-        timidity_string = create_timidity_tuning(self.scale, self.reference_note)
-        with open(file_path, 'w') as file_handle:
-            file_handle.write(timidity_string)
-        return True
